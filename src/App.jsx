21356617bd7930e44b6a8874417f1beb670f3fb9
Himanshu <himanshu290304@gmail.com>
2025-02-20 14:10:08 +0530

    modified App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,9 @@ import './App.css'
 function App() {
 
   return (
-    
-    <div className='grid grid-cols-12'>
-      <div className=' sm:col-span-5 bg-blue-300 col-span-12' >child 1</div>
-      <div className='sm:col-span-5 bg-red-300 col-span-12'>child 2</div>
-      <div className='sm:col-span-2 bg-green-300 col-span-12'>child 3</div>
-    </div>
+   <div className='bg-blue-400 '>
+    hii
+   </div> 
 ) 
 }
 
