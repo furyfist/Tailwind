20ed0eac472f85d51f72e8ab6232acc6b5e1d063
Himanshu <himanshu290304@gmail.com>
2025-02-20 15:00:37 +0530

    made a boiler plate of website

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
-
 import './App.css'
+
 
 function App() {
 
   return (
-   <div className='bg-blue-400 '>
-    hii
+   <div className=' bg-blue-900' >
+       <div className='container flex  flex-col items-center justify-center min-h-screen space-y-4' >
+          <div className='logo flex' >
+              <img src="" alt="logo here" />
+              <p>  - webinar.gg</p>
+          </div>
+
+          <div className='Verify-age'>
+            <p>Verify your age</p>
+
+          </div>
+
+          <div className='confirm date birth date'>
+            <p>Please confirm your Age</p>
+
+          </div>
+
+          <div className='Input Dob flex flex-col space-y-1'>
+            <input type="text" placeholder='Enter Your Birth Date' />
+            <button className='bg-green-600'>Continue</button>
+          </div>
+       </div>
    </div> 
 ) 
 }
